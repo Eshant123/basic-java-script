8541500341012aff40b87918f6b38f9b07da7502
Eshant123 <71774275+Eshant123@users.noreply.github.com>
2024-06-28 08:14:30 +0000

    variables

---
+++ 01-basic/variables.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="eshant@gmail.com"
+var accuntcity="bangluru"
+
+//accountId=2345
+
+accountEmail="nistjant@gmail.com"
+accuntcity="pune"
+console.log(accountId)
+console.table([accountId,accountEmail,accuntcity])
+//prefer not use var keyword because of isssue block scope and functional scope
+//local varibale:-java script local variable create inside block or function
+//Gloabal vairibale:- java script golabal varaibale accissable by any function and gloabal variable decleared outside the function or decleared with window object is known as gloabal vaibale
