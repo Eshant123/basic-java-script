5f8e76077bb2928b0edc2f31c863a360ffdc2b8c
Eshant123 <71774275+Eshant123@users.noreply.github.com>
2024-06-29 04:55:36 +0000

    datatypes

---
+++ 01-basic/datatypes.js
@@ -0,0 +1,14 @@
+let score= true
+console.log(typeof null)
+console.log(typeof (score))
+let valuInNumber=Number(score)
+console.log(typeof valuInNumber)
+console.log(valuInNumber)
+let isLoggedIn=null
+let booleanInNumber=Boolean(isLoggedIn)
+console.log(booleanInNumber)
+
+let someNumber=33
+let stringInNumber=String(someNumber)
+console.log(stringInNumber)
+console.log(typeof stringInNumber)
