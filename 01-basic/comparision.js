b26fb07efef4dc99d3b35bf53ac513af375de2d8
Eshant123 <71774275+Eshant123@users.noreply.github.com>
2024-06-29 08:32:29 +0000

    strings learnig

---
+++ 01-basic/comparision.js
@@ -0,0 +1,27 @@
+// 
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null==0)
+// console.log("4"===4)
+// console.log(4===4)
+//primitive 
+// string, number,boolean,null undefined,symbol,Bigint
+//refrence non primitive
+// array obejct function
+// let number =100
+// let string="eshant"
+let myYoutubeName="eshantyadavdotcom"
+let anotherName=myYoutubeName
+anotherName="chai or code"
+console.log(anotherName)
+console.log(myYoutubeName)
+
+
+let userOne={
+    email:"yadavaeshant@gmail.com",
+    upi:"eshant@ybl"
+}
+let userTwo=userOne
+userTwo.email="chaiorcode@gmail"
+console.log(userTwo)
+console.log(userOne)
---
+++ 01-basic/string.js
@@ -0,0 +1,16 @@
+//string:- javascript string is an object that represent a sequence of character
+const name="eshant"
+const repocunt=50
+// console.log(name+" "+ repocunt)
+console.log(`my name is ${name} and repocount is ${repocunt}`)
+
+
+const gameName=new String("eshant")
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf("n"))
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+var str="Javatpoint";  
+console.log(str.substring(2,6))
+var str3=" Learn Node.js on Javatpoint. Node.js is a well-known JavaScript framework.";
+console.log(str3.replace("Node.js","Angular.js"))   
